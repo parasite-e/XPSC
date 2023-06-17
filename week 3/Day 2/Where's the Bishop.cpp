839afc05ef426d9b2165cf6e48b5d71daf30a016
parasite-e <fardinemonbd@gmail.com>
2023-06-17 22:03:45 +0600

    w3d2p5

---
+++ week 3/Day 2/Where's the Bishop.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        char arr[8][8] = {0};
+
+        for(int i = 0; i<8; i++)
+        {
+            for(int j = 0; j< 8; j++)
+            {
+                cin >> arr[i][j];
+            }
+        }
+
+         for(int i = 0; i<8; i++)
+        {
+            for(int j = 0; j< 8; j++)
+            {
+                if(arr[i][j] == '#')
+                {
+                    if((arr[i-1][j-1] == '#') && (arr[i-1][j+1] == '#') && (arr[i+1][j-1] == '#') && (arr[i+1][j+1] == '#'))
+                    {
+                        cout << i+1 << " " << j+1 << endl;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
