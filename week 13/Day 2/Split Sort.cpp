4e2de3c598e6b6779f30138b8994f07963c448cd
parasite-e <fardinemonbd@gmail.com>
2023-09-03 00:15:23 +0600

    .

--- week 13/Day 2/Split Sort.cpp
+++ week 13/Day 2/Split Sort.cpp
@@ -1,1 +1,31 @@
+#include<bits/stdc++.h>
 
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        map<int, int>mp;
+        for(int i = 0; i<n; i++)
+        {
+            cin >> arr[i];
+
+            mp[arr[i]] = i;
+        }
+        int cnt = 0;
+
+        for(int i = 2; i<=n; i++)
+        {
+            if(mp[i] < mp[i-1])cnt++;
+        }
+
+        cout << cnt << endl;
+    }
+}
